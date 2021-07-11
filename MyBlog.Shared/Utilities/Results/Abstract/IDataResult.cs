c1309caae260ffd8da4f46aa69fa1492ba0cb1a9
Emre Büyüktaş <emrebyk348@gmail.com>
2021-07-11 23:30:43 +0300

    Some refactoring

--- MyBlog.Shared/Utilities/Results/Abstract/IDataResult.cs
+++ MyBlog.Shared/Utilities/Results/Abstract/IDataResult.cs
@@ -9,7 +9,5 @@ namespace MyBlog.Shared.Utilities.Results.Abstract
     public interface IDataResult<out T>:IResult
     {
         public T Data { get; }//new DataResult<Category>(ResultStatus.Success,category);
-
-        public static implicit operator int(IDataResult<int> v);
     }
 }
